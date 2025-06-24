5f1980cfec5575159ae19b84c9e61ed81964fcaf
Morshed Shoan <shoanbd95@gmail.com>
2025-06-24 22:19:27 +0600

    Delete

--- Multiplication Table using for loop.py
+++
@@ -1,9 +0,0 @@
-while True:
-    num = int(input("Enter a number : "))
-    mul = 1
-    for i in range(1, 11):
-        mul = num * i
-        if (i == 10):
-            print(f"{num} * {i} : {mul}\n")
-            break
-        print(f"{num} * {i}  : {mul}")
