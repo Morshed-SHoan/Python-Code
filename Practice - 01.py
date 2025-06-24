18762cd24a067dce3e86133c0c5db778fb1df5bf
Morshed Shoan <shoanbd95@gmail.com>
2025-06-24 22:30:33 +0600

    new

---
+++ Practice - 01.py
@@ -0,0 +1,12 @@
+a = {
+    "Name" : ["Golam Morshed Shoan","Lamia Ferdous"],
+    "Age"  : [23,23],
+    "Phone" : {
+        "gp":'01798111671',
+    "Airtel":"01611230696"
+    }
+
+}
+
+
+print(a)
